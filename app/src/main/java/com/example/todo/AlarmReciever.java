3603435b66603185ce9de82ca83186bee5e5d993
vishali <vishali.cnr@gmail.com>
2020-01-10 18:32:35 +0530

    third commit

--- app/src/main/java/com/example/todo/AlarmReciever.java
+++ app/src/main/java/com/example/todo/AlarmReciever.java
@@ -9,15 +9,13 @@ import android.content.Intent;
 
 import com.example.todo.MainActivity;
 
-/**
- * Created by sara on 2017/12/28.
- */
+
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        // Get id & message from intent.
+
         int notificationId = intent.getIntExtra("notificationId", 0);
         String message = intent.getStringExtra("todo");
 
@@ -28,7 +26,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         NotificationManager myNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        // Prepare notification.
+
         Notification.Builder builder = new Notification.Builder(context);
         builder.setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setContentTitle("It's Time!")
@@ -39,7 +37,7 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setPriority(Notification.PRIORITY_MAX)
                 .setDefaults(Notification.DEFAULT_ALL);
 
-        // Notify
+
         myNotificationManager.notify(notificationId, builder.build());
 
     }
